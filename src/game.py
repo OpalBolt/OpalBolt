1ec43a548112ec45ce94650be900b142c708f226
OpalBolt <git@skumnet.dk>
2025-05-18 13:32:11 +0200

    feat: seperate player and oponent logic

--- src/game.py
+++ src/game.py
@@ -38,37 +38,72 @@ from player_logic import player_play_round
 starting_dice = 6
 app = typer.Typer()
 
-@app.command()
-def roll_player(game_id: int, player: str, dice: int):
-    pass
 
 @app.command()
-def roll_oponent(game_id: int = None):
-    
+def roll_player(game_id: int, player: str, dice: int):
+    game_meta = load_meta()
+
     if game_id:
         game = load_game_data(game_id)
     else:
-        game = game_data(
-            game_score_player=0,
-            game_score_opponent=0,
+        game_id = game_meta.game_id
+        game = load_game_data(game_meta.game_id)
+
+    current_round = load_round_data(game_meta.game_id, game_meta.round_id)
+
+    if game_meta.turn_id == 0:
+        current_turn = player_play_round(starting_dice, game_meta, player)
+    else:
+        _tmp_meta = game_meta
+        _tmp_meta.turn_id -= 1
+        last_turn = load_turn_data(_tmp_meta)
+        current_turn = player_play_round(
+            last_turn.avalible_dice, game_meta=game_meta, player=player
         )
 
-    current_round = oponent_play_round(starting_dice)
+
+def make_checks(game_meta: meta_data, game: game_data, current_round: round_data):
+    check_game_end(game)
+    save_game_data(game, game_meta.game_id)
+    save_round_data(current_round, game_meta.game_id, game_meta.round_id)
+    game_meta.round_id += 1
+    save_meta(game_meta)
+
+    if game.game_ended:
+        game_meta.game_id += 1
+        game_meta.round_id = 0
+        game.game_score_player = 0
+        game.game_score_opponent = 0
+        save_meta(game_meta)
+        save_game_data(game, game_meta.game_id)
+
+
+@app.command()
+def roll_game(game_id: Optional[int] = None, player: str = "oponent"):
+    game_meta = load_meta()
+
+    if game_id:
+        game = load_game_data(game_id)
+    else:
+        game = load_game_data(game_meta.game_id)
+
+    if player != "oponent":
+        player_play_round(
+            num_dice_input=starting_dice, game_meta=game_meta, player=player
+        )
+    else:
+        current_round = oponent_play_round(starting_dice)
 
     game.game_score_opponent += current_round.round_score
-    print(game)
-    
-    check_game_end(game)
 
+    make_checks(game_meta, game, current_round)
 
-    save_game_data(game, 1)
-    save_round_data(current_round, 1, 1)
-
-    
     return game
+
 
 def main():
     app()
+
 
 if __name__ == "__main__":
     main()
