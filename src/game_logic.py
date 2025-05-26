5bd2bc925be6e4e0d67314db2e03757dd89efe41
OpalBolt <git@skumnet.dk>
2025-05-26 21:27:35 +0200

    feat: Update logic to keep track of dice

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,10 +1,11 @@
 import random
 from collections import Counter
-from models import turn_data, game_data
+from typing import Optional
+from models import round_data, turn_data, game_data
 
 
 def calculate_score_roll(
-    dice: list, turn: turn_data, dice_to_keep: list = []
+    dice: list, turn: turn_data, dice_to_keep: Optional[list] = []
 ) -> turn_data:
     print(f"Dice rolled: {dice}")
     _keep = False
@@ -25,36 +26,38 @@ def calculate_score_roll(
     turn.score = 0
 
     if len(unique_numbers) == 6:
-        turn.next_roll_dice = 6
+        turn.dice_left = 6
         turn.score = 1500
         return turn
 
     if len(dice) == 6 and values.count(2) == 3:
-        turn.next_roll_dice = 6
+        turn.dice_left = 6
         turn.score = 1000
         return turn
 
     # Count each number
     for num in counts:
         count = counts[num]
+        turn.dice_left -= count
 
         if count >= 3:
+            print("3 pairs")
             base_score = 1000 if num == 1 else num * 100
             multiplier = 2 ** (count - 3)  # 3 of a kind = x1, 4 = x2, 5 = x4, 6 = x8
             turn.score += base_score * multiplier
-            turn.next_roll_dice -= count
             count -= 3  # Remove the three used for the set
 
         # Singles of 1s or 5s after sets
         if num == 1:
             print("1")
             turn.score += count * 100
-            turn.next_roll_dice -= count
 
         elif num == 5:
             print("5")
             turn.score += count * 50
-            turn.next_roll_dice -= count
+
+    if turn.dice_left == 0:
+        turn.dice_left = 6
 
     return turn
 
