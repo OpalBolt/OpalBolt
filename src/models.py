cc3d58601f10548e4e5e6df9a013544bab52230e
OpalBolt <git@skumnet.dk>
2025-06-04 08:37:25 +0200

    feat: re-create dataclass as pydantic basemodel

--- src/models.py
+++ src/models.py
@@ -1,32 +1,38 @@
-from dataclasses import dataclass
+from pydantic import BaseModel, Field
 
 
-@dataclass
-class game_data:
-    game_score_player: int
-    game_score_opponent: int
-    game_ended: bool = False
 
 
-@dataclass
-class turn_data:
-    avalible_dice: list
-    next_roll_dice: int
-    score: int
-    used_dice: int
 
 
-@dataclass
-class round_data:
-    player: str
-    round_score: int
-    turn: list
-    rolls: list
-    dice_left: int
+
+class game_data(persistableJson):
+    save_path: str = Field(default="game", repr=False)
+    game_score_player: int = Field(default=0)
+    game_score_opponent: int = Field(default=0)
+    game_ended: bool = Field(default=False)
 
 
-@dataclass
-class meta_data:
-    game_id: int
-    round_id: int
-    turn_id: int
+class turn_data(persistableJson):
+    save_path: str = Field(default="turn", repr=False)
+    avalible_dice: list = Field(default=[])
+    score: int = Field(default=0)
+    dice_left: int = Field(default=0)
+
+
+class round_data(persistableJson):
+    save_path: str = Field(default="round", repr=False)
+    next_roll_dice: int = Field(default=6)
+    player: str = Field(default="")
+    round_score: int = Field(default=0)
+    rolls: list = Field(default=[])
+    dice_left: int = Field(default=0)
+    end: bool = Field(default=False)
+
+
+class meta_data(persistableJson):
+    save_path: str = Field(default="meta", repr=False)
+    game_id: int = Field(default=0)
+    round_id: int = Field(default=0)
+    turn_id: int = Field(default=0)
+    avalible_dice: list = Field(default=[])
