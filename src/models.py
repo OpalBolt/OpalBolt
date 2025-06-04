e06a4fe416da50248ea52d046f5dd836babef082
OpalBolt <git@skumnet.dk>
2025-06-04 08:37:38 +0200

    feat: implement save and load logic

--- src/models.py
+++ src/models.py
@@ -1,9 +1,43 @@
 from pydantic import BaseModel, Field
+from pathlib import Path
+import json
 
 
+class persistableJson(BaseModel):
+    save_path: str = Field(default="", repr=False)
 
+    def _file_path(self, additional_values: str = "") -> Path:
+        return (
+            Path(__file__).parent.parent
+            / self.save_path
+            / f"{self.save_path}_data{additional_values}.json"
+        )
 
+    def save(self, additional_values: str = ""):
+        file = (
+            self._file_path(additional_values)
+            if additional_values
+            else self._file_path()
+        )
+        file.parent.mkdir(parents=True, exist_ok=True)
+        data = self.model_dump()
+        data.pop("save_path", None)
+        data.pop("save_file_name", None)
+        file.write_text(json.dumps(data, ensure_ascii=False))
 
+    @classmethod
+    def load(cls, additional_values: str = ""):
+        inst = cls()
+        file = (
+            inst._file_path(additional_values)
+            if additional_values
+            else inst._file_path()
+        )
+        # print(f"trying to load {file}")  # DEBUG
+        # print(file.read_text())
+        if file.exists():
+            return cls.model_validate_json(file.read_text())
+        return cls()
 
 
 class game_data(persistableJson):
