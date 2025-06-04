1148ea2350f9f04ce367463797437aaf592e13bd
OpalBolt <git@skumnet.dk>
2025-06-04 08:38:45 +0200

    feat: fix logic regarding playing a round

--- src/oponent_logic.py
+++ src/oponent_logic.py
@@ -13,32 +13,26 @@ def oponent_rolls():
     return _strat_rolls
 
 
-def oponent_play_round(num_dice_input: int) -> round_data:
+def oponent_play_round(num_dice_input: int, current_round: round_data) -> round_data:
     rolls = oponent_rolls()
     log.debug(f"Oponent is detirmened to roll {rolls} times")
 
     turn = turn_data(
-        next_roll_dice=num_dice_input,
         avalible_dice=roll_dice(num_dice_input),
         score=0,
-        used_dice=0,
+        dice_left=0,
     )
 
-    current_round = round_data(
-        player="oponent", round_score=0, turn=[], rolls=[], dice_left=0
-    )
     log.debug(turn)
 
     for _ in range(rolls):
         turn = calculate_score_roll(turn.avalible_dice, turn)
-
         # Check if the turn score is 0
         # If the turn score is 0, the round ends and the round score is set to 0
         if turn.score == 0:
             log.debug("oponent roll came up with a score of 0, ending round")
             current_round.round_score = 0
             current_round.rolls += [turn]
-
             break
 
         # Add the turn to the round rolls, and add the score to the round score
@@ -47,12 +41,12 @@ def oponent_play_round(num_dice_input: int) -> round_data:
 
         # Check if the oponent has scored more than 300
         # and if the next roll is less than 3 dice
-        if turn.next_roll_dice <= 3 and current_round.round_score >= 300:
+        if current_round.next_roll_dice <= 3 or current_round.round_score >= 300:
             log.debug("round ended, high score or few dice left")
             break
 
-        turn.avalible_dice = roll_dice(turn.next_roll_dice)
+        turn.avalible_dice = roll_dice(current_round.next_roll_dice)
 
-    return current_round
     log.debug(f"oponent scored: {current_round.round_score} points this round")
 
+    return current_round
