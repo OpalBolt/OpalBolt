21bc5a1d6dfb17fd190651b48e4f35a11c174497
OpalBolt <git@skumnet.dk>
2025-06-04 08:39:34 +0200

    feat: Update turn logic

--- src/player_logic.py
+++ src/player_logic.py
@@ -6,32 +6,29 @@ from game_logic import calculate_score_roll, find_dice_to_keep
 
 
 def player_play_round(
-    num_dice_input: int, game_meta: meta_data, player: str
-) -> turn_data:
+    num_dice_input: int,
+    current_round: round_data,
+    dice_to_keep: Optional[int] = None,
+) -> round_data:
     turn = turn_data(
-        next_roll_dice=num_dice_input,
         avalible_dice=roll_dice(num_dice_input),
         score=0,
-        used_dice=0,
+        dice_left=0,
     )
 
-    current_round = load_round_data(game_meta.game_id, game_meta.round_id)
-
-    turn = calculate_score_roll(turn.avalible_dice, turn)
+    turn = calculate_score_roll(
+        turn.avalible_dice, turn, find_dice_to_keep(dice_to_keep)
+    )
 
     if turn.score == 0:
         current_round.round_score = 0
         current_round.rolls += [turn]
+        current_round.end = True
 
-        return turn
+        return current_round
 
         # Add the turn to the round rolls, and add the score to the round score
     current_round.rolls += [turn]
     current_round.round_score += turn.score
 
-    # Check if the oponent has scored more than 300
-    # and if the next roll is less than 3 dice
-    if turn.next_roll_dice <= 3 and current_round.round_score >= 300:
-        return turn
-
-    return turn
+    return current_round
